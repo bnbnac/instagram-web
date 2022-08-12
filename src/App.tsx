156812f4a7814fcb4c66bb9b3ce16a8dcecfc146
bnbnac <bnbnac@naver.com>
2022-08-12 10:27:14 +0900

    Router - notfound and redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+
 function App() {
-  return <div></div>;
+  const isLoggedIn = false;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+          <Route path="*" element={<NotFound />} />
+          {/* <Route path="*" element={<Navigate to="/" />} /> /// REDIRECTING */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="react-scripts" />
+
+declare function Routes(props: RoutesProps): React.ReactElement | null;
+
+interface RoutesProps {
+  children?: React.ReactNode;
+  location?: Partial<Location> | string;
+}
+
+declare function Route(props: RouteProps): React.ReactElement | null;
+
+interface RouteProps {
+  caseSensitive?: boolean;
+  children?: React.ReactNode;
+  element?: React.ReactNode | null;
+  index?: boolean;
+  path?: string;
+}
