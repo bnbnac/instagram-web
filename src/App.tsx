0db5138facf75746c4d870373f9952ce519bceea
bnbnac <bnbnac@naver.com>
2022-08-13 18:49:06 +0900

    ThemeProvider. in this case, props comes from App-ThemeProvider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import { useReactiveVar } from "@apollo/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { isLoggedInVar } from "./apollo";
+import { ThemeProvider } from "styled-components";
+import { darkModeVar, isLoggedInVar } from "./apollo";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 
+const lightTheme = {
+  fontColor: "#133965",
+  bgColor: "lightgray",
+};
+const darkTheme = {
+  fontColor: "lightgray",
+  bgColor: "#133965",
+};
+
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <div>
-      <BrowserRouter>
+    <BrowserRouter>
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
         <Routes>
           <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
